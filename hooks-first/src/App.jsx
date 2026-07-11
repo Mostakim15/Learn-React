e10f5763a9f3d13cd3b96f62c66626f8ef7d5ea3
Mostakim15 <mbmostakim5050@gmail.com>
2026-07-12 01:46:32 +0600

    Hooks in react

---
+++ hooks-first/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  let value = 10;
+  let [count, setCount] = useState(value);
+
+  const addValue = () =>{
+    console.log("increase",count);
+    setCount(count+1);
+  }
+
+  const reduceValue =() =>
+  {
+    console.log("decrease",count);
+    setCount(--count);
+    console.log("decrease after",count);
+  }
+
+  return (
+    <>
+     <h1>Count react</h1>
+     <h2>actual value {value}</h2>
+     <br />
+     <h2>count value {count}</h2>
+     <button onClick={addValue}>
+    Add value
+     </button>
+<br />
+     <button onClick={reduceValue}>reduce value</button>
+    </>
+  )
+}
+
+export default App
