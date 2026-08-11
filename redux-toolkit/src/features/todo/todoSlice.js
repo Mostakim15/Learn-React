60aca257679ff12b884f04316363cb44e6aa8b4a
Mostakim15 <mbmostakim5050@gmail.com>
2026-08-11 23:59:32 +0600

    store

--- redux-toolkit/src/features/todo/todoSlice.js
+++ redux-toolkit/src/features/todo/todoSlice.js
@@ -21,7 +21,27 @@ export const todoSlice = createSlice({
             state.todos.push(todo); //state gives the current data situation in todos
         }, //state gives the current data situation in todos
         
-        removeTodo: (state, action) => {},//action gives the data that is passed to the reducer function
-        toggleTodo: (state, action) => {},
+        removeTodo: (state, action) => {
+            state.todos = state.todos.filter(todo => todo.id !== action.payload);
+        },//action gives the data that is passed to the reducer function
+        
+        updateTodo: (state, action) => {
+            const {id, text} = action.payload;
+            const existingTodo = state.todos.find(todo => todo.id === id);
+            if (existingTodo) {
+                existingTodo.text = text;
+            }
+        },
+
+        toggleTodo: (state, action) => {
+            const existingTodo = state.todos.find(todo => todo.id === action.payload);
+            if (existingTodo) {
+                existingTodo.completed = !existingTodo.completed;
+            }
+        }
     },
 });
+
+export const {addTodo, removeTodo, updateTodo, toggleTodo} = todoSlice.actions;
+
+export default todoSlice.reducer; //exporting the reducer function to be used in the stores
