5568333232c6783d187c288615f7cfa305e1da09
Mostakim15 <mbmostakim5050@gmail.com>
2026-08-21 20:59:51 +0600

    config

--- react-blog/src/appwrite/config.js
+++ react-blog/src/appwrite/config.js
@@ -14,7 +14,7 @@ export class Service{
         this.bucket = new Storage(this.client);
     }
 
-    async createPost({title, slug, content, featuredImage, status, userId}){
+    async createPost({title, slug, content, blogImage, status, userId}){
         try {
             return await this.databases.createDocument(
                 conf.appwriteDatabaseId,
@@ -23,7 +23,7 @@ export class Service{
                 {
                     title,
                     content,
-                    featuredImage,
+                    blogImage,
                     status,
                     userId,
                 }
@@ -33,7 +33,7 @@ export class Service{
         }
     }
 
-    async updatePost(slug, {title, content, featuredImage, status}){
+    async updatePost(slug, {title, content, blogImage, status}){
         try {
             return await this.databases.updateDocument(
                 conf.appwriteDatabaseId,
@@ -42,7 +42,7 @@ export class Service{
                 {
                     title,
                     content,
-                    featuredImage,
+                    blogImage,
                     status,
 
                 }
